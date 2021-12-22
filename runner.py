492d771903063e911e7f11328797773c56564034
Tobias Lindroth <31400061+rothen98@users.noreply.github.com>
2021-12-22 21:25:09 +0100

    Using stationary opponent

--- runner.py
+++ runner.py
@@ -11,11 +11,11 @@ def calculateMatrixGame(size):
 def run():
     gameSize = 16
     agent: Agent = Agent(10, 0.1, np.array([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]), gameSize)
-    for _ in range(1000):
+    for _ in range(100):
         (agentPayoff, opponentPayoff) = calculateMatrixGame(gameSize)
         agent.observeGame(agentPayoff, opponentPayoff)
         act = agent.pickMove()
-        oppAct = opponentPayoff.sum(axis=0).argmax()
+        oppAct = 1 #opponentPayoff.sum(axis=0).argmax()
         print("Reward: ", agentPayoff[act,oppAct])
         agent.learn(oppAct)
 
